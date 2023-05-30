754328a2a09dd7a24ba1c946986b2d2aec40c48a
akamaru10 <kanekivicia@gmail.com>
2023-05-30 19:30:50 -0300

    CODIGO: AULA 03

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,14 @@
 import pygame
+from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
+Y_DUNK = 344
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -29,9 +31,11 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump:
             self.dino_duck = False
             self.dino_jump = False
@@ -62,7 +66,7 @@ class Dinosaur:
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS + 35
+        self.dino_rect.y = Y_DUNK
         self.step_index += 1
 
     def draw(self, screen):
