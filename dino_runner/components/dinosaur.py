3d0cb9baea65aca92d885cd6021d98cf268f0a09
akamaru10 <kanekivicia@gmail.com>
2023-05-30 13:47:58 -0300

    ATIVIDADE : 01

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
@@ -15,6 +15,7 @@ class Dinosaur:
         self.step_index = 0
         self.jump_vel = JUMP_VEL
         self.dino_jump = False
+        self.dino_duck = False
         self.dino_run = True
 
     def update(self, user_input):
@@ -22,11 +23,17 @@ class Dinosaur:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
         elif not self.dino_jump:
+            self.dino_duck = False
             self.dino_jump = False
             self.dino_run = True
 
@@ -52,7 +59,11 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS + 35
+        self.step_index += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
