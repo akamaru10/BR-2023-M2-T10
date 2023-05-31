eeb479ee3eb7c821026f9be7a2439a5e476c96c8
akamaru10 <kanekivicia@gmail.com>
2023-05-31 17:12:00 -0300

    ATIVIDADE: AULA 03

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 
 class ObstacleManager:
@@ -10,14 +12,21 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            if random.random() < 0.8:
+                self.obstacles.append(Cactus((SMALL_CACTUS + LARGE_CACTUS)))
+            else:
+                self.obstacles.append(Bird())
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
+
+    def reset_obstacles(self):
+        self.obstacles = []
 
     def draw(self, screen):
         for obstacle in self.obstacles:
